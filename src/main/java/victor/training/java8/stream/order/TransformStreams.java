05e2623ed7115f1531680429f8b826ce72c072d7
Risto Uibo <risto.uibo@ignite.ee>
2021-03-18 16:29:58 +0200

    TransformStreams: remove unused imports

--- src/main/java/victor/training/java8/stream/order/TransformStreams.java
+++ src/main/java/victor/training/java8/stream/order/TransformStreams.java
@@ -1,34 +1,14 @@
 package victor.training.java8.stream.order;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.summingLong;
-import static java.util.stream.Collectors.toCollection;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
-import static java.util.stream.Collectors.toSet;
-
-import java.io.File;
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.nio.file.Files;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
-import java.util.function.Function;
-import java.util.stream.Stream;
-
 import victor.training.java8.stream.order.dto.OrderDto;
 import victor.training.java8.stream.order.entity.Customer;
 import victor.training.java8.stream.order.entity.Order;
+import victor.training.java8.stream.order.entity.Order.PaymentMethod;
 import victor.training.java8.stream.order.entity.OrderLine;
 import victor.training.java8.stream.order.entity.Product;
-import victor.training.java8.stream.order.entity.Order.PaymentMethod;
+
+import java.time.LocalDate;
+import java.util.*;
 
 public class TransformStreams {
 
