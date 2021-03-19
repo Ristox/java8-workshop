6d4a39f29de9f5d6090bff86915bed5a93dae47c
Risto Uibo <risto.uibo@ignite.ee>
2021-03-19 13:36:13 +0200

    DishPlay: group dishes by type and caloric level (failing / noncompiling test)

--- src/main/java/victor/training/java8/stream/menu/DishPlayTest.java
+++ src/main/java/victor/training/java8/stream/menu/DishPlayTest.java
@@ -2,10 +2,13 @@ package victor.training.java8.stream.menu;
 
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static victor.training.java8.stream.menu.Dish.Type.*;
 
 public class DishPlayTest {
 
@@ -72,5 +75,90 @@ public class DishPlayTest {
 
         assertEquals(expectedDishesContainingMeat, result);
     }
+
+    @Test
+    public void groupDishesByTypeAndCaloricLevel_listsExpectedGroupsOfDishes() {
+        Map<Dish.Type, Map<Object, List<Dish>>> expectedDishesGrouping = mapOf(
+            MEAT, mapOf(
+               800, list(DishPlay.menu.get(0)), //800 - [pork]
+               700, list(DishPlay.menu.get(1)), //700 - [beef]
+               400, list(DishPlay.menu.get(2)) //400 - [chicken]
+            ),
+            FISH, mapOf(
+                300, list(DishPlay.menu.get(7)), //300 - [prawns]
+                400, list(DishPlay.menu.get(8)) //400 - [salmon]
+            ),
+            OTHER, mapOf(
+                530, list(DishPlay.menu.get(3)), //530 - [french fries]
+                350, list(DishPlay.menu.get(4)), //350 - [rice]
+                350, list(DishPlay.menu.get(5)) //120 - [season fruit]
+            )
+        );
+
+        Map<Dish.Type, Map<Object, List<Dish>>> result = dishPlay.groupDishesByTypeAndCaloricLevel();
+
+        assertEquals(expectedDishesGrouping, result);
+    }
+
+    private <K, V> Map<K, V> mapOf(
+        K key1, V value1,
+        K key2, V value2
+    ) {
+        Map<K, V> map = new HashMap<>();
+        map.put(key1, value1);
+        map.put(key2, value2);
+        return map;
+    }
+
+    private <K, V> Map<K, V> mapOf(
+            K key1, V value1,
+            K key2, V value2,
+            K key3, V value3
+    ) {
+        Map<K, V> map = mapOf(
+            key1, value1,
+            key2, value2
+        );
+        map.put(key3, value3);
+        return map;
+    }
+
+    private <K, V> Map<K, V> mapOf(
+            K key1, V value1,
+            K key2, V value2,
+            K key3, V value3,
+            K key4, V value4
+    ) {
+        Map<K, V> map = mapOf(
+                key1, value1,
+                key2, value2,
+                key3, value3
+        );
+        map.put(key4, value4);
+        return map;
+    }
+
+    private <K, V> Map<K, V> mapOf(
+            K key1, V value1,
+            K key2, V value2,
+            K key3, V value3,
+            K key4, V value4,
+            K key5, V value5
+    ) {
+        Map<K, V> map = mapOf(
+                key1, value1,
+                key2, value2,
+                key3, value3,
+                key4, value4,
+        );
+        map.put(key5, value5);
+        return map;
+    }
+
+    private List<Dish> list(Dish... dishes) {
+        return asList(dishes);
+    }
+
+
 
 }
