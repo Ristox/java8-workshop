91e6d60e5f1e6660b421fb7a1a367ff746dde8da
Risto Uibo <risto.uibo@ignite.ee>
2021-03-22 12:52:44 +0200

    TransactionPlay: optimize imports

--- src/main/java/victor/training/java8/stream/transaction/TransactionPlayTest.java
+++ src/main/java/victor/training/java8/stream/transaction/TransactionPlayTest.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
-import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
