6f40938e3ebce9736f7b3df7d9500f17e041b93a
Risto Uibo <risto.uibo@ignite.ee>
2021-03-22 10:11:51 +0200

    TransactionPlay: implementation - all 2011 transactions sorted by value (pass test)

--- src/main/java/victor/training/java8/stream/transaction/TransactionPlay.java
+++ src/main/java/victor/training/java8/stream/transaction/TransactionPlay.java
@@ -3,11 +3,21 @@ package victor.training.java8.stream.transaction;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+
 public class TransactionPlay {
 
     private final List<Transaction> transactions;
 
     public TransactionPlay(List<Transaction> transactions) {
         this.transactions = new ArrayList<>(transactions);
+    }
+
+    public List<Transaction> sortAll2011TransactionsByValue() {
+        return transactions.stream()
+                .filter((tx) -> tx.getYear() == 2011)
+                .sorted(comparing(Transaction::getValue))
+                .collect(toList());
     }
 }
